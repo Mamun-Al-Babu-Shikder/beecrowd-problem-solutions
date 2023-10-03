2fb7ac6426d6a782412e04f485e85f52d1f24416
Mamun-Al-Babu-Shikder <49284765+Mamun-Al-Babu-Shikder@users.noreply.github.com>
2023-10-03 21:44:35 +0600

    beecrowd | 1005 Average 1
    Read two floating points' values of double precision A and B, corresponding to two student's grades. After this, calculate the student's average, considering that grade A has weight 3.5 and B has weight 7.5. Each grade can be from zero to ten, always with one digit after the decimal point. Don’t forget to print the end of line after the result, otherwise you will receive “Presentation Error”. Don’t forget the space before and after the equal sign.
    
    Input
    The input file contains 2 floating points' values with one digit after the decimal point.
    
    Output
    Print the message "MEDIA"(average in Portuguese) and the student's average according to the following example, with 5 digits after the decimal point and with a blank space before and after the equal signal.

---
+++ BEGINNER/BEE1005.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double media = (a * 3.5 + b * 7.5) / 11;
+        System.out.printf("MEDIA = %.5f\n", media);
+        sc.close();
+    }
+}
