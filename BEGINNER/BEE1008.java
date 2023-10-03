f39ca2fd94ce277f29b8dccb8a34a5ea7ea2f5cf
Mamun-Al-Babu-Shikder <49284765+Mamun-Al-Babu-Shikder@users.noreply.github.com>
2023-10-03 21:56:29 +0600

    beecrowd | 1008 Salary
    Write a program that reads an employee's number, his/her worked hours number in a month and the amount he received per hour. Print the employee's number and salary that he/she will receive at end of the month, with two decimal places.
    
    Don’t forget to print the line's end after the result, otherwise you will receive “Presentation Error”.
    Don’t forget the space before and after the equal signal and after the U$.
    Input
    The input file contains 2 integer numbers and 1 value of floating point, representing the number, worked hours amount and the amount the employee receives per worked hour.
    
    Output
    Print the number and the employee's salary, according to the given example, with a blank space before and after the equal signal.

---
+++ BEGINNER/BEE1008.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int employeeNumber = sc.nextInt();
+        int workedHours = sc.nextInt();
+        double amountPerHour = sc.nextDouble();
+        System.out.println("NUMBER = " + employeeNumber);
+        System.out.printf("SALARY = U$ %.2f\n", workedHours * amountPerHour);
+        sc.close();
+    }
+}
