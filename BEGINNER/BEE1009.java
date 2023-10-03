b2e7f54f5ce7f9a45f26f73ccf643c108e82eef3
Mamun-Al-Babu-Shikder <49284765+Mamun-Al-Babu-Shikder@users.noreply.github.com>
2023-10-03 22:04:13 +0600

    beecrowd | 1009 Salary with Bonus
    Make a program that reads a seller's name, his/her fixed salary and the sale's total made by himself/herself in the month (in money). Considering that this seller receives 15% over all products sold, write the final salary (total) of this seller at the end of the month , with two decimal places.
    
    - Don’t forget to print the line's end after the result, otherwise you will receive “Presentation Error”.
    
    - Don’t forget the blank spaces.
    
    Input
    The input file contains a text (employee's first name), and two double precision values, that are the seller's salary and the total value sold by him/her.
    
    Output
    Print the seller's total salary, according to the given example.

---
+++ BEGINNER/BEE1009.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String sellerName = sc.next();
+        double salary = sc.nextDouble();
+        double sold = sc.nextDouble();
+        System.out.printf("TOTAL = R$ %.2f\n", salary + ((sold * 15) / 100));
+        sc.close();
+    }
+}
