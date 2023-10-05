15654868a7ce2444ad4a57c641248ebafad620e6
Mamun-Al-Babu-Shikder <49284765+Mamun-Al-Babu-Shikder@users.noreply.github.com>
2023-10-05 18:44:26 +0600

    beecrowd | 1013 The Greatest
    Make a program that reads 3 integer values and present the greatest one followed by the message "eh o maior". Use the following formula:
    
    MaiorAB = (a+b+abs(a-b)) / 2
    
    Input
    The input file contains 3 integer values.
    
    Output
    Print the greatest of these three values followed by a space and the message “eh o maior”.

---
+++ BEGINNER/BEE1013.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        double maiorAB = (a + b + Math.abs(a - b)) / 2.0;
+        double greatest = (maiorAB + c + Math.abs(maiorAB - c)) / 2;
+        System.out.printf("%d eh o maior\n", (int) greatest);
+        sc.close();
+    }
+}
