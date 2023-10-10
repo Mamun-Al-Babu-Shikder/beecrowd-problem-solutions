6c186f458e26724469e3d56335f92e420933ba6a
Mamun-Al-Babu-Shikder <49284765+Mamun-Al-Babu-Shikder@users.noreply.github.com>
2023-10-10 20:36:02 +0600

    beecrowd | 1018 Banknotes
    In this problem you have to read an integer value and calculate the smallest possible number of banknotes in which the value may be decomposed. The possible banknotes are 100, 50, 20, 10, 5, 2 and 1. Print the read value and the list of banknotes.
    
    Input
    The input file contains an integer value N (0 < N < 1000000).
    
    Output
    Print the read number and the minimum quantity of each necessary banknotes in Portuguese language, as the given example. Do not forget to print the end of line after each line, otherwise you will receive “Presentation Error”.

---
+++ BEGINNER/BEE1018.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        int[] notes = {100, 50, 20, 10, 5, 2, 1};
+        Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+        System.out.println(value);
+        for (int note : notes) {
+            int n = value / note;
+            value = value % note;
+            System.out.printf("%d nota(s) de R$ %d,00\n", n, note);
+        }
+        sc.close();
+    }
+}
